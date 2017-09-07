6dccf1191139f88c03d8bdc8164e7ff2d928ced6
olikirkness <oli@kirkness.com>
2017-09-07 11:19:24 +0100

    player show styled

--- src/js/controllers/players/show.controller.js
+++ src/js/controllers/players/show.controller.js
@@ -22,7 +22,7 @@ function PlayerShowCtrl($stateParams, User) {
         {
           name: 'series-1',
           data:
-            vm.formattedMatches
+          vm.formattedMatches
         }]
       },{
         axisX: {
@@ -33,102 +33,79 @@ function PlayerShowCtrl($stateParams, User) {
           }
         }
       });
-    // var chart = new Chartist.Line('.ct-chart', {
-    //   series: [
-    //     {
-    //       name: 'series-1',
-    //       data: [
-    //         {x: new Date(143134652600), y: 53},
-    //         {x: new Date(143234652600), y: 40},
-    //         {x: new Date(143340052600), y: 45},
-    //         {x: new Date(143366652600), y: 40},
-    //         {x: new Date(143410652600), y: 20},
-    //         {x: new Date(143508652600), y: 32},
-    //         {x: new Date(143569652600), y: 18},
-    //         {x: new Date(143579652600), y: 11}
-    //       ]
-    //     },
-    //     {
-    //       name: 'series-2',
-    //       data: [
-    //         {x: new Date(143134652600), y: 53},
-    //         {x: new Date(143234652600), y: 35},
-    //         {x: new Date(143334652600), y: 30},
-    //         {x: new Date(143384652600), y: 30},
-    //         {x: new Date(143568652600), y: 10}
-    //       ]
-    //     }
-    //   ]
-    // }, {
-    //   axisX: {
-    //     type: Chartist.FixedScaleAxis,
-    //     divisor: 5,
-    //     labelInterpolationFnc: function(value) {
-    //       return moment(value).format('MMM D');
-    //     }
-    //   }
-    // });
 
-    var pieData = {
-      labels: ['Wins', 'Losses'],
-      series: [vm.player.matches_won, vm.player.matches.length - vm.player.matches_won]
-    };
 
-    var pieOptions = {
-      labelInterpolationFnc: function(value) {
-        return value[0];
-      }
-    };
+      var pieData = {
+        series: [vm.player.matches_won, vm.formattedMatches.length - 1 - vm.player.matches_won],
+        labels: [`Wins - ${Math.round(vm.player.matches_won/(vm.formattedMatches.length - 1 )*100)}%`, `Losses - ${Math.round((vm.formattedMatches.length - 1 - vm.player.matches_won)/(vm.formattedMatches.length - 1 )*100)}%`]
 
-    var responsiveOptions = [
-      ['screen and (min-width: 640px)', {
-        chartPadding: 30,
-        labelOffset: 100,
-        labelDirection: 'explode',
+      };
+
+      var pieOptions = {
         labelInterpolationFnc: function(value) {
-          return value;
+          return value[0];
         }
-      }],
-      ['screen and (min-width: 1024px)', {
-        labelOffset: 80,
-        chartPadding: 20
-      }]
-    ];
+      };
 
-    vm.pie = new Chartist.Pie('.pie', pieData, pieOptions, responsiveOptions);
+      var responsiveOptions = [
+        ['screen and (min-width: 640px)', {
+          chartPadding: 30,
+          labelOffset: 100,
+          labelDirection: 'explode',
+          labelInterpolationFnc: function(value) {
+            return value;
+          }
+        }],
+        ['screen and (min-width: 1024px)', {
+          labelOffset: 80,
+          chartPadding: 20
+        }]
+      ];
+    var sum = function(a, b) {
+      console.log(a,b);
+      return a + b;
+    };
+//
+
+      vm.pie = new Chartist.Pie('.pie', pieData, pieOptions, responsiveOptions);
+      // {
+      //   labelInterpolationFnc: function(value) {
+      //     console.log(value);
+      //     return Math.round(value / pieData.series.reduce(sum) * 100) + '%';
+      //   }
 
     vm.data = [vm.levels, vm.pie];
   });
-  //   var ctx = document.getElementById('myChart').getContext('2d');
-  //   ctx.canvas.width = 300;
-  //   ctx.canvas.height = 300;
+    //   var ctx = document.getElementById('myChart').getContext('2d');
+    //   ctx.canvas.width = 300;
+    //   ctx.canvas.height = 300;
 
-  //   ctx.canvas.parentNode.style.height = '128px';
-  //
-  //   // new Chart(ctx, {
-  //   //   type: 'line',
-  //   //   data: {
-  //   //     labels: vm.matchStamps,
-  //   //     datasets: [{
-  //   //       fill: true,
-  //   //       backgroundColor: '#BF6969',
-  //   //       label: 'Ranking',
-  //   //       data: vm.player.ranking.reverse()
-  //   //     }]
-  //   //   },
-  //   //   options: {
-  //   //     maintainAspectRatio: true,
-  //   //     layout: {
-  //   //       padding: {
-  //   //         left: 0,
-  //   //         right: 0,
-  //   //         top: 0,
-  //   //         bottom: 0
-  //   //       }
-  //   //     }
-  //   //   }
-  //   // });
-  // });
+    //   ctx.canvas.parentNode.style.height = '128px';
+    //
+    //   // new Chart(ctx, {
+    //   //   type: 'line',
+    //   //   data: {
+    //   //     labels: vm.matchStamps,
+    //   //     datasets: [{
+    //   //       fill: true,
+    //   //       backgroundColor: '#BF6969',
+    //   //       label: 'Ranking',
+    //   //       data: vm.player.ranking.reverse()
+    //   //     }]
+    //   //   },
+    //   //   options: {
+    //   //     maintainAspectRatio: true,
+    //   //     layout: {
+    //   //       padding: {
+    //   //         left: 0,
+    //   //         right: 0,
+    //   //         top: 0,
+    //   //         bottom: 0
+    //   //       }
+    //   //     }
+    //   //   }
+    //   // });
+    // });
 
 
-}
+  }
