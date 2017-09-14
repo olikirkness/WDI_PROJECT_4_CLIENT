1acea32e715678f339920885862dc2a655c53732
olikirkness <oli@kirkness.com>
2017-09-14 14:55:27 +0100

    changed constant file for heroku

--- src/js/constants/api.constants.js
+++ src/js/constants/api.constants.js
@@ -1,3 +1,4 @@
 angular
   .module('LeagueApp')
-  .constant('API', `http://localhost:3000/api`);
+  // .constant('API', `http://localhost:3000/api`);
+  .constant('API', `https://matchplayleagues.herokuapp.com/api`);
