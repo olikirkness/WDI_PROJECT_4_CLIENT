17eec8dc2dcedc529b8f8671470cbeecf2e9063a
olikirkness <oli@kirkness.com>
2017-09-18 19:49:46 +0100

    config for heroku

--- src/js/constants/api.constants.js
+++ src/js/constants/api.constants.js
@@ -1,4 +1,4 @@
 angular
   .module('LeagueApp')
-  .constant('API', `http://localhost:3000/api`);
-  // .constant('API', `https://matchplayleagues.herokuapp.com/api`);
+  // .constant('API', `http://localhost:3000/api`);
+  .constant('API', `https://matchplayleagues.herokuapp.com/api`);
